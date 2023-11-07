83c7485f17737a2e802c3816fbb11028bf2c7ead
Malek Abasise <abdmalek.abassisse@esprit.tn>
2023-11-07 22:18:00 +0100

    first commit

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const mongoose = require("./middleware/connect");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const userRoutes = require('./routes/user');
+
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/api/v1/user", userRoutes);
+
 app.listen(process.env.PORT || 8000, () => {
     console.log("Server up!");
 });
