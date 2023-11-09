e38bdfe8f203266b36b721e231a340f2f149e788
BaccarSabri2 <115673916+BaccarSabri2@users.noreply.github.com>
2023-11-10 00:43:43 +0100

    updated v2

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,15 @@ const router = express.Router();
 
 router.post(`/add`, userController.addUser);
 router.post(`/update`, userController.updateUser);
+router.post(`/checkRedirect`, userController.checkRedirect);
+router.post(`/updateRedirect`, userController.updateRedirect);
+router.post(`/getuser`, userController.getuser);
+
+
+
+
+
+
 
 module.exports = router;
 
